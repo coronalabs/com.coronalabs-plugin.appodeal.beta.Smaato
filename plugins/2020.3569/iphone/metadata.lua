d34abf0f942bc34d32859e19b848435d260b3a9d
Vlad Shcherban <vlad@coronalabs.com>
2020-04-14 00:03:35 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDSmaatoAdapter', },
+		frameworks = { 'OMSDK_Smaato', 'SmaatoSDKBanner', 'SmaatoSDKCore', 'SmaatoSDKInterstitial', 'SmaatoSDKOpenMeasurement', 'SmaatoSDKRewardedAds', 'SmaatoSDKRichMedia', 'SmaatoSDKUnifiedBidding', 'SmaatoSDKVideo', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
